8e2aa0793a97fe4b7a9ae51f85babbaf49a2b718
vk007 <vkchauhan877cool@gmail.com>
2025-12-16 02:26:16 +0530

    use env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SettingsModule } from './settings/settings.module';
 import { BlogCategoryModule } from './components/blog-category/blog-category.module';
 import { BlogModule } from './components/blog/blog.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'blog_management',
+      type: process.env.DB_TYPE as 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306, 
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
       //logging: true, 
